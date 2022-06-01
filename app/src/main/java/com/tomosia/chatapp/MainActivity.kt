c4bb31c5f2c85331dcda0897fedcf688126aee99
kieubaongoc <tt.ngoc.kieu@tomosia.com>
2022-06-01 14:29:07 +0700

    dev ui welcome-login-register

--- app/src/main/java/com/tomosia/chatapp/MainActivity.kt
+++ app/src/main/java/com/tomosia/chatapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.tomosia.chatapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.tomosia.chatapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.actNavHost)
+        return navController.navigateUp()
     }
 }
